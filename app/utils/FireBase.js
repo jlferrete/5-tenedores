e11e8c82721328194797092a875727545057716c
JlFerrete <joseluisferrete@gmail.com>
2019-07-28 19:15:52 +0200

    Firebase configuration

---
+++ app/utils/FireBase.js
@@ -0,0 +1,9 @@
+export default (firebaseConfig = {
+    apiKey: "AIzaSyD2fmBjFk3j4R-ezDSEABsJf0HwFmPaBX0",
+    authDomain: "tenedores-ee183.firebaseapp.com",
+    databaseURL: "https://tenedores-ee183.firebaseio.com",
+    projectId: "tenedores-ee183",
+    storageBucket: "tenedores-ee183.appspot.com",
+    messagingSenderId: "74965889105",
+    appId: "1:74965889105:web:8cacd4bab1131377"
+});
