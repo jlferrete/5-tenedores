ab757303093591671778c01119c7980f7ae3e072
JlFerrete <joseluisferrete@gmail.com>
2019-08-01 22:31:04 +0200

    Add login image

--- app/screens/MyAccount/Login.js
+++ app/screens/MyAccount/Login.js
@@ -1,10 +1,18 @@
 import React, { Component } from "react";
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, View, Text, ActivityIndicator } from 'react-native';
+
+import { Image } from "react-native-elements";
 
 export default class Login extends Component {
     render() {
         return (
             <View style={styles.viewBody}>
+                <Image
+                    source={require('../../../assets/img/5-tenedores-letras-icono-logo.png')}
+                    style={styles.logo}
+                    PlaceholderContent={<ActivityIndicator />}
+                    resizeMode="contain"
+                />
                 <Text>Login Screen...</Text>
             </View>
         );
@@ -14,9 +22,13 @@ export default class Login extends Component {
 const styles = StyleSheet.create({
     viewBody: {
         flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
         marginLeft: 40,
-        marginRight: 40
+        marginRight: 40,
+        marginTop: 40,
+        alignItems: "center"
+    },
+    logo: {
+        width: 300,
+        height: 150
     }
 });
