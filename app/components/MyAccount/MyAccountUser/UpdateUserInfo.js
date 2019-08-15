e320b855f7b6e3a28cc42a9b4736a68db823cd9e
JlFerrete <joseluisferrete@gmail.com>
2019-08-15 13:33:52 +0200

    Problem into UpdateUserInfo component

--- app/components/MyAccount/MyAccountUser/UpdateUserInfo.js
+++ app/components/MyAccount/MyAccountUser/UpdateUserInfo.js
@@ -1,12 +1,46 @@
 import React, { Component } from 'react';
 import { StyleSheet, View, Text } from 'react-native';
+import { ListItem } from "react-native-elements";
 
 export default class UpdateUserInfo extends Component {
 
+    constructor() {
+        super();
+
+        this.state = {
+            menuItems: [
+                {
+                    title: "Cambiar Nombre y Apellidos",
+                    iconType: "material-community",
+                    iconNameRight: "chevron-right",
+                    iconColorRight: "#ccc",
+                    iconNameLeft: "account-circle",
+                    iconColorLeft: "#ccc",
+                    onPress: () => console.log('Ha realizado click en Cambiar Nombre y Apellidos')
+                }
+            ]
+        };
+    }
     render() {
+        const { menuItems } = this.state;
         return (
             <View>
-                <Text>Update User Info...</Text>
+                {menuItems.map((item, index) => (
+                    <ListItem
+                        key={index}
+                        title={item.title}
+                        leftIcon={{
+                            type: item.iconType,
+                            name: item.iconNameLeft,
+                            color: item.iconColorLeft
+                        }}
+                        rightIcon={{
+                            type: item.iconType,
+                            name: item.iconNameRight,
+                            color: item.iconColorRight
+                        }}
+                    />
+                ))}
             </View>
         );
     }
